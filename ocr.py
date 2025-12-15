0a67b845a2fea3db42f12cb0cb6cee390e5a3177
TsThisPmoPissMeOff <deancameron279@gmail.com>
2025-12-14 20:54:09 -0500

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,13 +2,9 @@ import easyocr
 import cv2
 import numpy as np
 
-# Initialize EasyOCR reader once
 reader = easyocr.Reader(['en'])
 
 def extract_text(image_bytes):
-    """
-    Takes image bytes and returns extracted text using EasyOCR
-    """
     nparr = np.frombuffer(image_bytes, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     result = reader.readtext(img)
